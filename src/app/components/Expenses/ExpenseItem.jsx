5689eaab2f257fbd104515eee29e8cc2b3f3036f
Raul Samperio <raulsdev@gmail.com>
2023-06-08 18:00:48 -0600

    first commit

---
+++ src/app/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,25 @@
+'use client';
+import { useState } from 'react';
+
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('si se cambio bato!!');
+    console.log(title);
+  };
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>change title</button>
+    </div>
+  );
+}
+export default ExpenseItem;
